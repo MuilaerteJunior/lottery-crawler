ce0cbd79f863599e7a62adfa7bca605c540c63af
Rennan Toniolo Silva <renan.toniolo@hotmail.com>
2017-09-20 00:37:33 -0300

    init move from vssts

---
+++ LotteryCrawler/Crawlers/Mega.cs
@@ -0,0 +1,16 @@
+﻿using LotteryCrawler.Net;
+using LotteryCrawler.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LotteryCrawler.Crawlers
+{
+    public class Mega : Lottery
+    {
+        public Mega(): base("http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip" , "D_MEGA.HTM" , 6, 60)
+        {            
+        }        
+    }
+}
---
+++ LotteryCrawler/Model/Sorteio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryCrawler
+{
+    public class Sorteio
+    {
+        public Sorteio() {
+            this.Numbers = new List<int>();
+        }
+        public List<int> Numbers { get; set; }
+        public DateTime Date { get; set; }
+        public int Id { get; set; }
+    }
+}
